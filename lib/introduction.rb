4e420dd4338fdf7e7b9235eea342a0fd848a9456
virajransing <virajransing@gmail.com>
2019-08-21 16:00:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,10 @@ def introduction(name)
   puts "hi, my name is #{name}"
 end 
 
+def introduction_with_language(name, language)
+  puts "hi, my name is #{name} and I am learning to program in #{language}"
+end 
+
 
 
 
